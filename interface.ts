6d1f7005fc8d3fa9c70497a1395220f5fcb791a1
vivek lokhande <vivek2811999@gmail.com>
2022-02-26 16:11:20 +0530

    initial commit

---
+++ interface.ts
@@ -0,0 +1,17 @@
+interface User {
+  name: string;
+  id: number;
+}
+
+class UserAccount {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+const user = new UserAccount("Vivek", 3);
+console.log(user);
